7cae52bf347bd06c76dc76af2ab7d308744cc733
Luise Brito Jansen de Mello <luisebjmello.unilasalle@gmail.com>
2018-12-01 00:11:07 -0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,88 @@
+import React from "react"
+import { StyleSheet, Text, View, Image, Button } from "react-native"
+import { androidClientId } from "./superSecretKey"
+import Expo from "expo"
+
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      signedIn: false,
+      name: "",
+      photoUrl: ""
+    }
+  }
+  signIn = async () => {
+    try {
+      const result = await Expo.Google.logInAsync({
+        androidClientId:
+          "1019154852279-jb7vu77v2fq2b709jtm4sekb5ihgu4il.apps.googleusercontent.com",
+        scopes: ["profile", "email"]
+      })
+
+      if (result.type === "success") {
+        this.setState({
+          signedIn: true,
+          name: result.user.name,
+          photoUrl: result.user.photoUrl
+        })
+      } else {
+        console.log("cancelled")
+      }
+    } catch (e) {
+      console.log("error", e)
+    }
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        {this.state.signedIn ? (
+          <LoggedInPage name={this.state.name} photoUrl={this.state.photoUrl} />
+        ) : (
+          <LoginPage signIn={this.signIn} />
+        )}
+      </View>
+    )
+  }
+}
+
+const LoginPage = props => {
+  return (
+    <View>
+      <Text style={styles.header}>Entre com sua conta Google</Text>
+      <Text style={styles.header}></Text>
+      <Text style={styles.header}></Text>
+      <Button title="Efetuar Login" onPress={() => props.signIn()} />
+    </View>
+  )
+}
+
+const LoggedInPage = props => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>Bem-Vindo: {props.name}</Text>     
+      <Image style={styles.image} source={{ uri: props.photoUrl }} />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#ae5d75",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  header: {
+    fontSize: 25
+  },
+  image: {
+    marginTop: 15,
+    width: 150,
+    height: 150,
+    borderColor: "rgba(0,0,0,0.2)",
+    borderWidth: 3,
+    borderRadius: 150
+  }
+})
